03470ab41eef108a70a8f4971277a74f688f8a13
Khadim <khadim.bousso@gmail.com>
2021-04-14 16:58:35 +0200

    HTML CSS FORMULAIRE

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -16,34 +16,31 @@ class ContactController extends AbstractController
     public function index()
     {
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = array_map('trim', $_POST);
+            $errors = [];
+            $maxLength = 255;
 
-        $data = array_map('trim', $_POST);
-        $errors = [];
-        $maxLength = 255;
+            if (empty($data['name'])) {
+                $errors[] = 'Le prénom est obligatoire';
+            } elseif ($data['name'] > $maxLength) {
+                $errors = 'Le prénom doit faire moins de' . $maxLength . ' caractères';
+            }
 
-
-        if (empty($data['name'])) {
-            $errors[] = 'Le prénom est obligatoire';
-        }elseif ($data['name'] > $maxLength) {
-            $errors = 'Le prénom doit faire moins de' . $maxLength . ' caractères';
+            if (empty($data['email'])) {
+                $errors[] = 'L\'email est obligatoire';
+            }
+            if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+                $errors[] = 'Mauvais format d\'email';
+            }
+            if (empty($data['message'])) {
+                $errors[] = 'Un message est obligatoire';
+            }
+            if (empty($errors)) {
+                echo 'Tout à bien été rempli';
+            }
         }
-
-        if (empty($data['email'])) {
-            $errors[] = 'L\'email est obligatoire';
-        }        
-        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-            $errors[] = 'Mauvais format d\'email';
-        }
-        if (empty($data['message'])) {
-            $errors[] = 'Un message est obligatoire';
-        }
-        if(empty($errors)) {
-            echo 'Tout à bien été rempli';
-        }
-    }
 
         return $this->twig->render('Contact/contact.html.twig');
-        
     }
 }
