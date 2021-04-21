0de147a9aa57217af0c51c3a1f167c30f3a7c449
Khadim <khadim.bousso@gmail.com>
2021-04-21 19:51:15 +0200

    update ContactController

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -14,25 +14,25 @@ class ContactController extends AbstractController
      */
 
     private array $errors = [];
-
+    const MAX_LENGTH_NAME = 255;
+    const MAX_LENGTH_EMAIL = 320;
+    
     public function index()
     {
         $subjects = ['tel-session', 'cabinet-session', 'infos', 'other'];
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $data = array_map('trim', $_POST);
-            define('MAX_LENGTH_NAME', 255);
-            define('MAX_LENGTH_EMAIL', 320);
-
+            
             $this->checkSentenceEmpty($data['name'], 'Un nom complet est obligatoire');
-            $this->checkSentenceEmpty($data['tel'], 'Veuillez renseigner votre numéro de téléphone');
+            $this->checkSentenceEmpty($data['tel'], 'Un numéro de téléphone est obligatoire');
             $this->checkSentenceEmpty($data['message'], 'Un message est obligatoire');
             $this->checkSentenceEmpty($data['email'], 'L\'email est obligatoire');
 
-            $this->checkWordSize($data['name'], MAX_LENGTH_NAME, 'Le nom complet doit faire moins de '
-                . MAX_LENGTH_NAME . ' caractères');
-            $this->checkWordSize($data['email'], MAX_LENGTH_EMAIL, 'Votre adresse mail doit faire moins de '
-                . MAX_LENGTH_EMAIL . ' caractères');
+            $this->checkWordSize($data['name'], self::MAX_LENGTH_NAME , 'Le nom complet doit faire moins de '
+                . self::MAX_LENGTH_NAME . ' caractères');
+            $this->checkWordSize($data['email'], self::MAX_LENGTH_EMAIL, 'Votre adresse mail doit faire moins de '
+                . self::MAX_LENGTH_EMAIL . ' caractères');
 
             $this->checkWordPresenceInArray($data['subject'], $subjects, 'Le sujet saisi n\'est pas valable');
             $this->checkFilterValidateEmail($data['email'], 'Mauvais format d\'email');
@@ -45,28 +45,28 @@ class ContactController extends AbstractController
         return $this->twig->render('Contact/contact.html.twig', ['errors' => $this->getErrors()]);
     }
 
-    public function checkWordPresenceInArray(string $word, array $array, string $message)
+    private function checkWordPresenceInArray(string $word, array $array, string $message)
     {
         if (!in_array($word, $array)) {
             $this->setErrors($message);
         }
     }
 
-    public function checkWordSize(string $word, int $length, string $messageError)
+    private function checkWordSize(string $word, int $length, string $messageError)
     {
         if (strlen($word) > $length) {
             $this->setErrors($messageError);
         }
     }
 
-    public function checkFilterValidateEmail(string $sentence, string $messageError)
+    private function checkFilterValidateEmail(string $sentence, string $messageError)
     {
         if (!filter_var($sentence, FILTER_VALIDATE_EMAIL)) {
             $this->setErrors($messageError);
         }
     }
 
-    public function checkSentenceEmpty(string $sentences, string $messageError)
+    private function checkSentenceEmpty(string $sentences, string $messageError)
     {
         if (empty($sentences)) {
             $this->setErrors($messageError);
