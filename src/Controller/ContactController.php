61e2880e2178a8515456431236cda48608ed90e4
Khadim <khadim.bousso@gmail.com>
2021-04-21 15:07:40 +0200

    update css and html

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -24,50 +24,49 @@ class ContactController extends AbstractController
             define('MAX_LENGTH_NAME', 255);
             define('MAX_LENGTH_EMAIL', 320);
 
-            $this->SetCheckSentenceEmpty($data['name'], 'Un nom complet est obligatoire');
-            $this->SetCheckSentenceEmpty($data['tel'], 'Veuillez renseigner votre numéro de téléphone');
-            $this->SetCheckSentenceEmpty($data['message'], 'Un message est obligatoire');
-            $this->SetCheckSentenceEmpty($data['email'], 'L\'email est obligatoire');
+            $this->checkSentenceEmpty($data['name'], 'Un nom complet est obligatoire');
+            $this->checkSentenceEmpty($data['tel'], 'Veuillez renseigner votre numéro de téléphone');
+            $this->checkSentenceEmpty($data['message'], 'Un message est obligatoire');
+            $this->checkSentenceEmpty($data['email'], 'L\'email est obligatoire');
 
-            $this->setCheckWordSize($data['name'], MAX_LENGTH_NAME, 'Le nom complet doit faire moins de '
+            $this->checkWordSize($data['name'], MAX_LENGTH_NAME, 'Le nom complet doit faire moins de '
                 . MAX_LENGTH_NAME . ' caractères');
-            $this->setCheckWordSize($data['email'], MAX_LENGTH_EMAIL, 'Votre adresse mail doit faire moins de '
+            $this->checkWordSize($data['email'], MAX_LENGTH_EMAIL, 'Votre adresse mail doit faire moins de '
                 . MAX_LENGTH_EMAIL . ' caractères');
 
-            $this->setCheckWordPresenceInArray($data['subject'], $subjects, 'Le sujet saisie n\'est pas valable');
-            $this->setCheckFilterValidateEmail($data['email'], 'Mauvais format d\'email');
+            $this->checkWordPresenceInArray($data['subject'], $subjects, 'Le sujet saisi n\'est pas valable');
+            $this->checkFilterValidateEmail($data['email'], 'Mauvais format d\'email');
 
             if (empty($this->getErrors())) {
-                echo 'Votre message à bien été envoyé.';
+                return 'Votre message à bien été envoyé.';
             }
         }
 
         return $this->twig->render('Contact/contact.html.twig', ['errors' => $this->getErrors()]);
     }
 
-    public function setCheckWordPresenceInArray(string $word, array $array, string $message)
+    public function checkWordPresenceInArray(string $word, array $array, string $message)
     {
-        var_dump($word);
         if (!in_array($word, $array)) {
             $this->setErrors($message);
         }
     }
 
-    public function setCheckWordSize(string $word, int $length, string $messageError)
+    public function checkWordSize(string $word, int $length, string $messageError)
     {
         if (strlen($word) > $length) {
             $this->setErrors($messageError);
         }
     }
 
-    public function setCheckFilterValidateEmail(string $sentence, string $messageError)
+    public function checkFilterValidateEmail(string $sentence, string $messageError)
     {
         if (!filter_var($sentence, FILTER_VALIDATE_EMAIL)) {
             $this->setErrors($messageError);
         }
     }
 
-    public function setCheckSentenceEmpty(string $sentences, string $messageError)
+    public function checkSentenceEmpty(string $sentences, string $messageError)
     {
         if (empty($sentences)) {
             $this->setErrors($messageError);
