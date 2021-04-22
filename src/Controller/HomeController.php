c670f329806eea4eda1e73c1b826ee03beae5de4
ap <pouillart.alexandre@hotmail.fr>
2021-04-22 15:18:00 +0200

    Twig + DB + View Formation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,26 +1,18 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: aurelwcs
- * Date: 08/04/19
- * Time: 18:40
- */
-
 namespace App\Controller;
+
+use App\Model\PrestationManager;
 
 class HomeController extends AbstractController
 {
-    /**
-     * Display home page
-     *
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
     public function index()
     {
-        return $this->twig->render('Home/index.html.twig');
+        $prestationManager = new PrestationManager();
+        $prestations = $prestationManager->selectAll();
+            
+        return $this->twig->render('Home/index.html.twig', [
+            'prestations' => $prestations,
+        ]);
     }
 }
