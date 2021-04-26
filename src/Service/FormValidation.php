f9c13ac6afb76b32f33be58282100d1cfa9d26f6
aurelien <vannier.autrelien2@gmail.com>
2021-04-26 17:08:07 +0200

    correction of wording and deletion of readme

--- src/Service/FormValidation.php
+++ src/Service/FormValidation.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Services;
+namespace App\Service;
 
 class FormValidation
 {
-    protected array $errors = [];
+    private array $errors = [];
 
     public function sentenceEmpty(string $sentences, string $messageError): bool
     {
         if (empty($sentences)) {
-            $this->setErrors($messageError);
+            $this->addError($messageError);
             return true;
         } else {
             return false;
@@ -19,7 +19,7 @@ class FormValidation
     public function wordIsInArray(string $word, array $array, string $message): void
     {
         if (!in_array($word, $array)) {
-            $this->setErrors($message);
+            $this->addError($message);
         }
     }
 
@@ -33,25 +33,25 @@ class FormValidation
     public function emailFilterValidate(string $sentence, string $messageError): void
     {
         if (!filter_var($sentence, FILTER_VALIDATE_EMAIL)) {
-            $this->setErrors($messageError);
+            $this->addError($messageError);
         }
     }
 
     public function wordMaxSize(string $word, int $length, string $messageError): void
     {
         if (strlen($word) > $length) {
-            $this->setErrors($messageError);
+            $this->addError($messageError);
         }
     }
 
     public function wordMinSize(string $word, int $length, string $messageError): void
     {
         if (strlen($word) < $length) {
-            $this->setErrors($messageError);
+            $this->addError($messageError);
         }
     }
 
-    private function setErrors(string $errors): void
+    private function addError(string $errors): void
     {
         $this->errors[] = $errors;
     }
@@ -59,23 +59,5 @@ class FormValidation
     public function getErrors(): array
     {
         return $this->errors;
-    }
-
-    public function isNotErrors(): bool
-    {
-        if (empty($this->errors)) {
-            return true;
-        }
-        return false;
-    }
-
-    public function displayErrorsHtml(): string
-    {
-        $html = '<ul>';
-        foreach ($this->getErrors() as $error) {
-            $html .= '<li>' . $error . '</li>';
-        }
-        $html .= '</ul>';
-        return $html;
     }
 }
