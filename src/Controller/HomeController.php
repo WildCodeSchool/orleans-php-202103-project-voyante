72daf68688c8bce4b1bb527bcb701073efd0f20c
ap <pouillart.alexandre@hotmail.fr>
2021-04-26 16:29:48 +0200

    View Training

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,12 @@
 
 namespace App\Controller;
 
-use App\Model\PrestationManager;
+use App\Model\ServicesManager;
 
 class HomeController extends AbstractController
 {
     public function index()
     {
-        $prestationManager = new PrestationManager();
-        $prestations = $prestationManager->selectAll();
-        return $this->twig->render('Home/index.html.twig', [
-            'prestations' => $prestations,
-        ]);
+        return $this->twig->render('Home/index.html.twig');
     }
 }
