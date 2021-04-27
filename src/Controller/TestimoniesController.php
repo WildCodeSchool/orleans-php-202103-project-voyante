da2a8a84fd2c7005c628efbc221f2a5170673cf6
aurelien <vannier.autrelien2@gmail.com>
2021-04-27 16:37:35 +0200

    creat testimoniesController with edit_add_delete

--- src/Controller/TestimoniesController.php
+++ src/Controller/TestimoniesController.php
@@ -25,4 +25,57 @@ class TestimoniesController extends AbstractController
         $testimony = $testimoniesManager->selectOneById($id);
         return $this->twig->render('Admin/Testimony/show.html.twig', ['testimony' => $testimony]);
     }
+
+    /**
+     * Edit a specific Testimony
+     */
+    public function edit(int $id): string
+    {
+        $testimoniesManager = new TestimoniesManager();
+        $testimony = $testimoniesManager->selectOneById($id);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $testimony = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, update and redirection
+            $testimoniesManager->update($testimony);
+            header('Location: Admin/Testimony/show/' . $id);
+        }
+        return $this->twig->render('Admin/Testimony/edit.html.twig', ['testimony' => $testimony,]);
+    }
+
+    /**
+     * Add a new Testimony
+     */
+    public function add(): string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $testimony = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, insert and redirection
+            $testimoniesManager = new TestimoniesManager();
+            $id = $testimoniesManager->insert($testimony);
+            header('Location: /Admin/Testimony/show/' . $id);
+        }
+        return $this->twig->render('Admin/Testimony/add.html.twig');
+    }
+
+
+    /**
+     * Delete a specific Testimony
+     */
+    public function delete(int $id)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $testimoniesManager = new TestimoniesManager();
+            $testimoniesManager->delete($id);
+            header('Location: /Admin/Testimony/index');
+        }
+    }
 }
