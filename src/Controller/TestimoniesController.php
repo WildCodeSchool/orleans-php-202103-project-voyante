2ef564f98118981514d41faac904642ae21abb04
aurelien <vannier.autrelien2@gmail.com>
2021-04-28 22:51:42 +0200

    resolve conflict error

--- src/Controller/TestimoniesController.php
+++ src/Controller/TestimoniesController.php
@@ -17,4 +17,24 @@ class TestimoniesController extends AbstractController
             header('Location: /Admin/Testimony/index');
         }
     }
+    /**
+     * Edit a specific Testimony
+     */
+    public function edit(int $id): string
+    {
+        $testimoniesManager = new TestimoniesManager();
+        $testimony = $testimoniesManager->selectOneById($id);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $testimony = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, update and redirection
+            $testimoniesManager->update($testimony);
+            header('Location: Admin/Testimony/show/' . $id);
+        }
+        return $this->twig->render('Admin/Testimony/edit.html.twig', ['testimony' => $testimony,]);
+    }
 }
