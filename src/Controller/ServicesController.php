badc7589113f3da37c039e5d124a5983f97689cd
aurelien <vannier.autrelien2@gmail.com>
2021-04-28 23:15:18 +0200

    US13 admin ServicesController Index

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -6,5 +6,13 @@ use App\Model\ServicesManager;
 
 class ServicesController extends AbstractController
 {
-    // placer le code
+    /**
+     * List Services
+     */
+    public function index(): string
+    {
+        $servicesManager = new ServicesManager();
+        $services = $servicesManager->selectAll();
+        return $this->twig->render('Admin/Services/index.html.twig', ['services' => $services]);
+    }
 }
