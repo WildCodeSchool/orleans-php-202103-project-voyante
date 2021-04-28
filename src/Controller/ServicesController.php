7e12e85c6da9173ec484bc754c19c475b3297708
aurelien <vannier.autrelien2@gmail.com>
2021-04-28 23:18:25 +0200

    US13 admin ServicesController show

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -6,5 +6,13 @@ use App\Model\ServicesManager;
 
 class ServicesController extends AbstractController
 {
-    // placer le code
+    /**
+     * Show informations for a specific Testimony
+     */
+    public function show(int $id): string
+    {
+        $servicesManager = new ServicesManager();
+        $service = $servicesManager->selectOneById($id);
+        return $this->twig->render('Admin/Services/show.html.twig', ['service' => $service]);
+    }
 }
