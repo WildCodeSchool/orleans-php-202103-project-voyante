cbf6b4ff42e8b6debea262e0c40dbb1177698549
aurelien <vannier.autrelien2@gmail.com>
2021-04-28 23:31:04 +0200

    US13 admin ServicesController delete

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -6,5 +6,12 @@ use App\Model\ServicesManager;
 
 class ServicesController extends AbstractController
 {
-    // placer le code
+    public function delete(int $id)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $servicesManager = new ServicesManager();
+            $servicesManager->delete($id);
+            header('Location: /Admin/Services/index');
+        }
+    }
 }
