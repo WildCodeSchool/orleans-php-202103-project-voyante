4ebfbc057b85726e05738e36c2ae5719e487794a
aurelien <vannier.autrelien2@gmail.com>
2021-04-28 22:43:58 +0200

    conflict error

--- src/Controller/TestimoniesController.php
+++ src/Controller/TestimoniesController.php
@@ -17,4 +17,13 @@ class TestimoniesController extends AbstractController
             header('Location: /Admin/Testimony/index');
         }
     }
+    /**
+     * Show informations for a specific Testimony
+     */
+    public function show(int $id): string
+    {
+        $testimoniesManager = new TestimoniesManager();
+        $testimony = $testimoniesManager->selectOneById($id);
+        return $this->twig->render('Admin/Testimony/show.html.twig', ['testimony' => $testimony]);
+    }
 }
