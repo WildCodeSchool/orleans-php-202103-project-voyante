954259c2885be996cb7eb2e42770c9b4fb4cae1e
aurelien <vannier.autrelien2@gmail.com>
2021-04-28 23:36:53 +0200

    comment correction and bindValue

--- src/Model/ServicesManager.php
+++ src/Model/ServicesManager.php
@@ -5,9 +5,7 @@ namespace App\Model;
 class ServicesManager extends AbstractManager
 {
     public const TABLE = 'service';
-    /**
-     * Update item in database
-     */
+
     public function update(array $item): bool
     {
         $query = "UPDATE " . self::TABLE . " SET 'name'=:name, 'description'=:description, 
@@ -15,8 +13,8 @@ class ServicesManager extends AbstractManager
         $statement = $this->pdo->prepare($query);
         $statement->bindValue('name', $item['name'], \PDO::PARAM_STR);
         $statement->bindValue('description', $item['description'], \PDO::PARAM_STR);
-        $statement->bindValue('price1hour', $item['price1hour'], \PDO::PARAM_STR);
-        $statement->bindValue('price30min', $item['price30min'], \PDO::PARAM_BOOL);
+        $statement->bindValue('price1hour', $item['price1hour'], \PDO::PARAM_INT);
+        $statement->bindValue('price30min', $item['price30min'], \PDO::PARAM_INT);
 
         return $statement->execute();
     }
