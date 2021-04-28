63685025023082ccb25f1d02ee8a9621228c6cc1
aurelien <vannier.autrelien2@gmail.com>
2021-04-28 23:28:29 +0200

    US13 admin ServicesController add

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -6,5 +6,22 @@ use App\Model\ServicesManager;
 
 class ServicesController extends AbstractController
 {
-    // placer le code
+    /**
+     * Add a new Services
+     */
+    public function add(): string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $service = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, insert and redirection
+            $servicesManager = new ServicesManager();
+            $id = $servicesManager->insert($service);
+            header('Location: /Admin/Services/show/' . $id);
+        }
+        return $this->twig->render('Admin/Services/add.html.twig');
+    }
 }
--- src/Model/ServicesManager.php
+++ src/Model/ServicesManager.php
@@ -5,4 +5,17 @@ namespace App\Model;
 class ServicesManager extends AbstractManager
 {
     public const TABLE = 'service';
+
+    public function insert(array $item): int
+    {
+        $query = "INSERT INTO " . self::TABLE . "('name', 'description', 'price1hour', 'price30min') 
+                VALUES (:name, :description, :price1hour, :price30min)";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('name', $item['name'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $item['description'], \PDO::PARAM_STR);
+        $statement->bindValue('price1hour', $item['price1hour'], \PDO::PARAM_INT);
+        $statement->bindValue('price30min', $item['price30min'], \PDO::PARAM_INT);
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
 }
