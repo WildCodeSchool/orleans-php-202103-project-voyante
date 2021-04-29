2185981906c7646d93bdd2e1d56ffc3bd648c9fc
aurelien <vannier.autrelien2@gmail.com>
2021-04-29 15:41:00 +0200

    simplification of code

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,25 +6,7 @@ use App\Model\ServicesManager;
 
 class AdminController extends AbstractController
 {
-    /**
-     * Display home page
-     *
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
-    public function index(): string
-    {
-        return $this->twig->render('Admin/Home/index.html.twig');
-    }
-
-    public function editService(): string
-    {
-        return $this->twig->render('Admin/Services/edit_service.html.twig');
-    }
-
-    public function deleteService(int $id): void
+    public function deleteService($id): void
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $servicesManager = new ServicesManager();
