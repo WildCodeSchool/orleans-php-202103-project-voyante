2bef9902ffa98a5d41e968d4259a1c61e2992111
aurelien <vannier.autrelien2@gmail.com>
2021-04-30 16:22:27 +0200

    depart addvalidation testimony

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -53,4 +53,19 @@ class AdminController extends AbstractController
         }
         return $this->twig->render('Admin/Services/edit_service.html.twig');
     }
+    public function testimonyaddValidation(): string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $testimony = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, insert and redirection
+            $testimoniesManager = new TestimoniesManager();
+            $id = $testimoniesManager->insert($testimony);
+            header('Location: /Admin/Testimony/show/' . $id);
+        }
+        return $this->twig->render('Admin/Testimony/add.html.twig');
+    }
 }
