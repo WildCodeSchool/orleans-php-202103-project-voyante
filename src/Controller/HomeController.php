3d7e13ec07a16d77d9755d9457c5577459c2a253
Khadim <khadim.bousso@gmail.com>
2021-05-04 09:12:33 +0200

    boucle testimony on carousel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,15 +3,20 @@
 namespace App\Controller;
 
 use App\Model\ServicesManager;
+use App\Model\TestimoniesManager;
 
 class HomeController extends AbstractController
 {
     public function index()
     {
+        $testimoniesManager = new TestimoniesManager();
+        $testimonies = $testimoniesManager->selectAll();
+
         $servicesManager = new ServicesManager();
         $services = $servicesManager->selectAll();
         return $this->twig->render('Visitor/Home/index.html.twig', [
             'services' => $services,
+            'testimonies' => $testimonies
         ]);
     }
 
