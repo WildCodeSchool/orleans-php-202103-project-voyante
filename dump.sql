ff15def6e6fc0687d17d5ebd0511f092f3f7703c
aurelien <vannier.autrelien2@gmail.com>
2021-05-06 10:20:29 +0200

    fix spelling mistakes

--- dump.sql
+++ dump.sql
@@ -1,11 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- SQL Dump
 --
 -- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
+-- Le :  Jeu 6 mai 2021
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -15,11 +12,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
--- Base de données :  projet_voyance
---
-
--- --------------------------------------------------------
-
+-- 
 -- Structure de la table `service`
 --
 
@@ -36,14 +29,13 @@ CREATE TABLE IF NOT EXISTS `service` (
 --
 
 INSERT INTO `service` (`name`, `description`, `price1hour`) VALUES
-("Au cabinet", "Pendant le rendez-vous nous pratiquons le tirage de carte de tarot, la lecture des lignes de la main, l'astrologie avancée et la lecture de votre avenir.", 75);
+("Au cabinet", "Pendant le rendez-vous nous pratiquons le tirage de cartes de tarot, la lecture des lignes de la main, l'astrologie avancée et la lecture de votre avenir.", 75);
 INSERT INTO `service` (`name`, `description`, `price1hour`, `price30min`) VALUES
-("À distance", "Nous pratiquons la même prestation qu'en rendez-vous au cabinet, le tout adapté pour lire votre avenir de manière toute aussi précise. Consultation par téléphone ou visioconférence.", 75, 50);
+("À distance", "Nous pratiquons la même prestation qu'en rendez-vous au cabinet, le tout adapté pour lire votre avenir de manière tout aussi précise. Consultation par téléphone ou visioconférence.", 75, 50);
+
+
 
 -------------------------------------------------------------
---
--- Base de données :  `testimony`
---
 --
 -- Structure de la table `testimony`
 --
